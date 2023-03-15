b2298d4eacc25088c9027537ac5ae81528379ba3
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2023-03-15 17:54:00 -0600

    Added add-book button

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+const addBook = document.getElementById('add-book');
+
+
+
 function openNav() {
     document.getElementById("mySidebar").style.width = "20vw";
     document.getElementById("main").style.marginLeft = "20vw";
