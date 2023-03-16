f5d74b47c32be3be15a07dcfad9cc2d98135bfc6
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2023-03-16 17:40:33 -0600

    Centered form & added JS book function

--- javascript.js
+++ javascript.js
@@ -16,6 +16,8 @@ function removeBook() {
 
 }
 
+const formAddBook = document.getElementById('form-add-book');
+
 function validateForm() {
     const title = document.getElementById('book-title');
     const author = document.getElementById('author');
@@ -32,56 +34,36 @@ function closeAddBook() {
 
 
 // Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close-form")[0];
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-form")[0];
-
-// When the user clicks the button, open the modal 
+// When the user clicks the add button, open form 
 addBook.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// Close the modal
 span.onclick = function() {
     modal.style.display = "none";
+    mainContainer.style.filter = "blur(2px)"
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == addBookContainer) {
-        addBookContainer.style.display = "none";
+    if (event.target == modal) {
+        modal.style.display = "none";
         mainContainer.style.filter = "none";
     }
 }
 
+const book1 = newBook(title, author, published, pages, readOrNot);
 
+function Book(title, author, published, pages, readOrNot) {
+    this.title = title;
+    this.author = author;
+    this.published = published;
+    this.pages = pages;
+    this.readOrNot = readOrNot;
+   }
+// books come from user from form
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-  mainContainer.style.filter = "blur(2px)"
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-  mainContainer.style.filter = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-    mainContainer.style.filter = "none";
-  }
-}
